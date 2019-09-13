3d6d49a3a92db28b9da56fbb007e80e43b8aa9e9
Lukas Lazarek <LukasALazarek@gmail.com>
2019-09-13 15:11:13 -0500

    Add test fest script

---
+++ test-fest.rkt
@@ -0,0 +1,38 @@
+#lang at-exp racket
+
+(require racket/cmdline)
+
+
+(define student-test-repos
+  '("git@github.com:NorthwesternSoftwareConstructionFall19/dummy-team-tests.git"))
+
+(define assign-number "0")
+(define student-path "./student")
+
+(command-line
+ #:once-each
+ [("-a" "--assign")
+  assign-number*
+  "Assignment number"
+  (set! assign-number assign-number*)]
+ [("-s" "--student")
+  student-path*
+  "Path to student exe"
+  (set! student-path student-path*)])
+
+(define test-repos-dir "./student-tests")
+(delete-directory/files test-repos-dir
+                        #:must-exist? #f)
+(make-directory test-repos-dir)
+(parameterize ([current-directory test-repos-dir])
+  (for ([repo (in-list student-test-repos)]
+        [i (in-naturals)])
+    (define this-student-tests @~a{student-tests-@i})
+    (system @~a{git clone "@repo" @this-student-tests})
+    (for ([test (in-directory (build-path this-student-tests assign-number))])
+      (cond [(not (system @~a{../@|i|/main @test}))
+             (displayln @~a{Test @test invalid})]
+            [(not (system @~a{@student-path @test}))
+             (displayln @~a{Failed test @test})]
+            [else
+             (displayln @~a{Passed test @test})]))))
