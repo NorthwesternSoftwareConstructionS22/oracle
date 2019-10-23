7c26fb6f2aa866a2f5ed6505d1a48ccf3c78e2c8
Lukas Lazarek <LukasALazarek@gmail.com>
2019-10-23 17:18:25 -0500

    grading-snapshot: switch to using and auto-managed env file

---
+++ test-fest/env.rkt
@@ -0,0 +1,18 @@
+#lang at-exp racket
+
+(provide write-env!)
+
+(require "util.rkt"
+         "test-fest-data.rkt")
+
+(define/contract (write-env! dir env-file team assign-number)
+  (path-to-existant-directory? string? string? assign-number? . -> . any)
+
+  (display-to-file @~a{
+                       export ASSIGN_MAJOR=@(car assign-number)
+                       export ASSIGN_MINOR=@(cdr assign-number)
+                       export TEAM_NAME=@team
+
+                       }
+                   (build-path dir env-file)
+                   #:exists 'truncate))
