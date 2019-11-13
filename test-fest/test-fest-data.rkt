d578be647dda17d19ab1488a78c86351ac81d2b7
Lukas Lazarek <LukasALazarek@gmail.com>
2019-11-13 09:04:10 -0600

    fest: Use pre-validated tests on wed & thu

--- test-fest/test-fest-data.rkt
+++ test-fest/test-fest-data.rkt
@@ -91,7 +91,7 @@
 
 
 ;; sunday is 0, saturday is 6
-(define pre-validated-test-days '(1 2)) ;; monday and tuesday
+(define pre-validated-test-days '(1 2 3 4)) ;; monday - thursday
 (define (use-pre-validated-tests?)
   (define week-day (->wday (today #:tz "America/Chicago")))
   (log-fest debug
