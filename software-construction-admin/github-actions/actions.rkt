f3a1f4ca7667ef51a77ddcc038403010aa6cd447
Lukas Lazarek <LukasALazarek@gmail.com>
2021-03-23 13:35:45 -0500

    admin: finish necessary actions api support

--- software-construction-admin/github-actions/actions.rkt
+++ software-construction-admin/github-actions/actions.rkt
@@ -1,15 +1,16 @@
 #lang at-exp racket
 
 (provide get-all-runs!
+         get-runs-by-url!
          cancel-run!
-         ;; restart-run!
-         ;; get-run-log!
          launch-run!
-         )
+         get-run-log!
+         (struct-out ci-run))
 
 (require "github-api.rkt"
          "../common/option.rkt"
          "../common/git.rkt"
+         "../common/util.rkt"
          gregor
          file/unzip)
 
@@ -23,13 +24,33 @@
                 cancel-url)
   #:prefab)
 
-(define (get-all-runs! repo-owner repo-name)
+(define/contract (get-all-runs! repo-owner repo-name)
+  (string? string? . -> . (option/c (listof ci-run?)))
+
   (option-let*
    [run-info (github-request! (~a "repos/" repo-owner "/" repo-name "/actions/runs"))]
    [runs (hash-ref/option run-info
                           'workflow_runs
                           @~a{Failed to get runs: @run-info})]
    (map json->ci-run runs)))
+
+(define/contract (get-runs-by-url! repo-owner repo-name urls)
+  (string? string? (listof string?) . -> . (option/c (hash/c string? (option/c ci-run?))))
+
+  (option-let*
+   [all-runs (get-all-runs! repo-owner repo-name)]
+   [runs-by-url (for/hash ([run (in-list all-runs)])
+                  (values (ci-run-url run) run))]
+   (for/hash ([url (in-list urls)])
+     (values url
+             (hash-ref/option runs-by-url url @~a{No run found with url @url})))))
+
+(define (failed-to action code headers in)
+  (failure
+   @~a{
+       Failed to @action, response code @code
+       Message: @(try-read-body-string code headers in)
+       }))
 
 (define (json->ci-run run-info-json)
   (match run-info-json
@@ -53,27 +74,16 @@
     [else (failure "Unexpected run info shape in api response")]))
 
 (define/contract (cancel-run! a-run)
-  (ci-run? . -> . any)
+  (ci-run? . -> . boolean?)
 
   (github-request! (ci-run-cancel-url a-run)
                    #:method POST
-                   #:read-response void))
-
-;; lltodo: wiw:
-;; - Modify github-request! to
-;;   1. Return the status code as well as calling read-response
-;;   2. Only call read-response if the content-length is not 0, otherwise produce another arg value
-;; - Play with the etag header on listing runs to see if it will only return if a there's a new run or what. It might also return if any runs status changes?
-;;   If it only returns on new run/status change, we can poll without needing to worry too much I think.
-;;   + Checked out etag and it doesn't work like that: it will give a new thing pretty much every time when there's an actively running job
-
-
-;; idea: have this function launch a sequence of runs, and then another function that can return the launches for those sequences. The second function will just do some slow polling (once per 5sec) until it has all the runs.
+                   #:read-response (λ (code headers in) (equal? code 202))))
 
 (define branch-name? string?)
-
 (define run-retrieval-polling-period-seconds 5)
 (define run-retrieval-polling-timeout-seconds (* 1 60))
+
 (define/contract (launch-run! repo-owner repo-name workflow-id ref)
   (string?
    string?
@@ -88,13 +98,16 @@
       (match (get-all-runs! repo-owner repo-name)
         [(present current-jobs)
          #:when (> (length current-jobs) original-count)
-         (set-first (set-subtract current-jobs original-jobs))]
+         (present (set-first (set-subtract current-jobs original-jobs)))]
         [else
          #:when (< (* retry-count run-retrieval-polling-period-seconds)
                    run-retrieval-polling-timeout-seconds)
          (sleep run-retrieval-polling-period-seconds)
          (loop (add1 retry-count))]
-        [else (failure "Couldn't find launched job")])))
+        [else (failure @~a{
+                           Couldn't find launched job after polling for @;
+                           @|run-retrieval-polling-timeout-seconds|s
+                           })])))
 
   (option-let*
    [jobs-before-launch (get-all-runs! repo-owner repo-name)]
@@ -102,14 +115,81 @@
                            "/actions/workflows/" workflow-id "/dispatches")
                        #:method POST
                        #:data (string->bytes/utf-8 @~a{{"ref": "@ref"}})
-                       #:read-response
-                       (match-lambda** [{204 _}    (present 'ok)]
-                                       [{other in}
-                                        (failure (if (sync/timeout 0 in)
-                                                     (failure (~a other ": " (port->string in)))
-                                                     (failure (~a "response code " other))))]))]
+                       #:read-response (match-lambda**
+                                        [{204 _ _} (present 'ok)]
+                                        [{other headers in}
+                                         (failed-to "launch run"
+                                                    other
+                                                    headers
+                                                    in)]))]
    [the-run (wait/poll-for-new-job! jobs-before-launch)]
    the-run))
 
-#;(define (get-run-log! a-run)
-    use `unzip` here, do a little experimenting with it)
+(define/contract wget
+  path-to-existant-file?
+  (find-executable-path "wget"))
+
+(define/contract (get-run-log! a-run #:section [section-name #f])
+  ({ci-run?}
+   {#:section (or/c string? #f)}
+   . ->* .
+   (option/c string?))
+
+  (define (url->log-contents log-download-url)
+    (match-define (list stdout stdin _ stderr ctl)
+      (process* wget
+                "-O"
+                "-"
+                log-download-url))
+    (close-output-port stdin)
+
+    (define log-sections (box empty))
+    (unzip
+     stdout
+     (match-lambda**
+      [{(regexp #rx"^.*/([0-9]+)_(.+).txt$" (list _
+                                                  section-number-bytes
+                                                  section-name-bytes))
+        #f
+        contents-port}
+       #:when (or (not section-name)
+                  (string-ci=? (bytes->string/utf-8 section-name-bytes) section-name))
+       ;; Sections aren't necessarily unpacked in order, so hang on to the section number
+       ;; to order them later
+       (define section-number (string->number (bytes->string/utf-8 section-number-bytes)))
+       (set-box! log-sections
+                 (cons (list section-number (port->string contents-port))
+                       (unbox log-sections)))]
+      [{_ _ _} (void)]))
+    (ctl 'kill)
+    (close-input-port stdout)
+    (close-input-port stderr)
+
+    (match (unbox log-sections)
+      ['() (failure "Failed to get log using download url")]
+      [sections
+       (present (string-join (map second (sort sections < #:key first))
+                             "\n"))]))
+
+  (option-let*
+   [log-download-url (github-request!
+                      (ci-run-log-url a-run)
+                      #:read-response (match-lambda**
+                                       [{302 (regexp #rx"(?mi:^location: (.+?)$)" (list _ url)) _}
+                                        url]
+                                       [{302 headers in}
+                                        (failed-to @~a{
+                                                       parse log download url in headers:
+                                                       @headers
+                                                       }
+                                                   302
+                                                   headers
+                                                   in)]
+                                       [{other headers in}
+                                        (failed-to "get log download url"
+                                                   other
+                                                   headers
+                                                   in)]))]
+   [contents (url->log-contents log-download-url)]
+   contents))
+
--- software-construction-admin/github-actions/github-api.rkt
+++ software-construction-admin/github-actions/github-api.rkt
@@ -2,6 +2,7 @@
 
 (provide request!
          try-read-json
+         try-read-body-string
          github-request!
          current-github-token
          GET POST)
@@ -16,7 +17,7 @@
 
 (define (request! url
                   headers
-                  [read-output (λ (code in) (port->string in))]
+                  [read-output (λ (code header in) (port->string in))]
                   #:method [method GET]
                   #:data [post-data #f])
 
@@ -26,7 +27,7 @@
                    [(regexp @pregexp{HTTP/[^ ]* +(\d+)} (list _ code))
                     (string->number code)]
                    [else -1]))
-    (read-output code in))
+    (read-output code header in))
   (call/input-url (string->url url)
                   (λ (url)
                     (define req-f
@@ -37,28 +38,49 @@
                     (req-f url headers))
                   read-output/parse-status))
 
+(define api-url? #rx"^https://.+[^/]$")
 (define/contract current-github-api-base-url
-  (parameter/c #rx"^https://.+[^/]$")
+  (parameter/c api-url?)
   (make-parameter "https://api.github.com"))
 (define-runtime-path token-file "../../../../.gh-token.gpg")
 (define current-github-token
   (make-parameter (and (file-exists? token-file)
                        (system/string @~a{ gpg --batch -dq @token-file}))))
 
-(define (try-read-json code in)
+(define (try-read-json code headers in)
   (define in-str (port->string in))
   (with-handlers ([exn:fail:read? (λ _ in-str)])
     (call-with-input-string in-str read-json)))
 
-(define (github-request! request-url
-                         #:output-type [output-type "application/vnd.github.v3+json"]
-                         #:auth-token [token (current-github-token)]
-                         #:base-url [base-url (current-github-api-base-url)]
-                         #:extra-headers [extra-headers '()]
-                         #:method [method GET]
-                         #:data [post-data #f]
-                         #:read-response [read-response try-read-json]
-                         #:user-agent [user-agent "software-construction-admin"])
+(define (try-read-body-string code headers in)
+  (match headers
+    [(regexp #rx"(?mi:^content-length: (.+?)$)"
+             (list _ (app string->number (? integer? len))))
+     #:when (> len 0)
+     (port->string in)]
+    [else "<no message body>"]))
+
+(define/contract (github-request! request-url
+                                  #:output-type [output-type "application/vnd.github.v3+json"]
+                                  #:auth-token [token (current-github-token)]
+                                  #:base-url [base-url (current-github-api-base-url)]
+                                  #:extra-headers [extra-headers '()]
+                                  #:method [method GET]
+                                  #:data [post-data #f]
+                                  #:read-response [read-response try-read-json]
+                                  #:user-agent [user-agent "software-construction-admin"])
+  ({string?}
+   {#:output-type string?
+    #:auth-token string?
+    #:base-url api-url?
+    #:extra-headers (listof (and/c string? #rx".+: .+"))
+    #:method (or/c GET POST)
+    #:data bytes?
+    #:read-response (integer? string? input-port? . -> . any)
+    #:user-agent string?}
+   . ->* .
+   any)
+
   (request!
    (if (string-prefix? request-url "http")
        request-url
