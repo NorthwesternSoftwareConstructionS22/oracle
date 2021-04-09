6284fe659ecb2d3c6b1581f75021b08736feaf99
Lukas Lazarek <LukasALazarek@gmail.com>
2021-04-09 10:14:40 -0500

    admin/test-novelty: check that tests are unique within teams

--- software-construction-admin/test-validation/test-novelty.rkt
+++ software-construction-admin/test-validation/test-novelty.rkt
@@ -12,11 +12,20 @@
 (define/contract (filter-already-submitted-tests all-tests assign-number)
   ((listof test/c) assign-number? . -> . (listof test/c))
 
+  (log-fest-info "Checking that tests are unique.")
+  (define unique-tests (remove-duplicates-within-teams all-tests))
+  (unless (= (length unique-tests) (length all-tests))
+    (log-fest-error @~a{
+                        Filtering out duplicate tests: @;
+                        @(map (compose1 pretty-path test-input-file)
+                              (set-subtract all-tests unique-tests))
+                        }))
+
   (define assign-instructor-tests (get-instructor-tests assign-number))
   (log-fest-info "Checking tests for novelty against instructor tests.")
   (define tests-different-than-instructor
     (filter-not (same-input-exists-in assign-instructor-tests)
-                all-tests))
+                unique-tests))
 
   (cond [(member assign-number assigns-conflicting-with-past-tests)
          (log-fest-info "Checking tests for novelty against previous assignments.")
@@ -42,10 +51,10 @@
   (filter instructor-test?
           (directory->tests (assign-number->validated-tests-path assign-number))))
 
+(define (test->input-json a-test)
+  (call-with-input-file (test-input-file a-test)
+    read-json*/safe))
 (define (same-input-exists-in tests)
-  (define (test->input-json a-test)
-    (call-with-input-file (test-input-file a-test)
-      read-json*/safe))
   (define test-inputs (list->set (map test->input-json tests)))
   (λ (a-test)
     (define found? (set-member? test-inputs (test->input-json a-test)))
@@ -57,4 +66,13 @@
            or in a past assignment.
            }))
     found?))
+
+(define (remove-duplicates-within-teams all-tests)
+  (define (test->team-name a-test)
+    (and (has-validated-test-input-file-naming-convention? (test-input-file a-test))
+         (validated-test-input-file->team-name (test-input-file a-test))))
+  (define grouped (group-by test->team-name all-tests))
+  (flatten (for/list ([team-tests (in-list grouped)])
+             (remove-duplicates team-tests
+                                #:key test->input-json))))
 
