2b27ac4eaa47c56a09fcaf2aaabb9fc74487cc93
Robby Findler <robby@racket-lang.org>
2022-03-27 15:17:20 -0500

    7.2 and sync oracle

--- software-construction-admin/utils/install-instructor-tests.rkt
+++ software-construction-admin/utils/install-instructor-tests.rkt
@@ -13,7 +13,7 @@
     (delete-directory/files dir))
   (make-directory* dir))
 
-(for* ([assign '(("3" . "1") ("3" . "2") ("4" . "1") ("5" . "1") ("5" . "2") ("6" . "1") ("7" . "1"))]
+(for* ([assign '(("3" . "1") ("3" . "2") ("4" . "1") ("5" . "1") ("5" . "2") ("6" . "1") ("7" . "1") ("7" . "2"))]
        [dest (in-value (build-path validated-dir (assign-number->dir-path-part assign)))]
        #:when (ensure-empty! dest) ; for side effect only
        [f (in-list (directory-list (build-path robbys-dir (assign-number->dir-path-part assign))
