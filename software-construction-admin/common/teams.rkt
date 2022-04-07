2638e3f473f107a33842c17cbc4f0df8bc65ead8
Lukas Lazarek <LukasALazarek@gmail.com>
2022-04-07 17:30:30 -0500

    Update team list

--- software-construction-admin/common/teams.rkt
+++ software-construction-admin/common/teams.rkt
@@ -10,8 +10,30 @@
 
 (define teams-1
   '("teamTest"
-    ;; example format: "team1"
-    ))
+    "team1"
+    "team2"
+    "team3"
+    "team4"
+    "team5"
+    "team6"
+    "team7"
+    "team8"
+    "team9"
+    "team10"
+    "team11"
+    "team12"
+    "team14"
+    "team15"
+    "team16"
+    "team17"
+    "team18"
+    "team19"
+    "team20"
+    "team21"
+    "team22"
+    "team23"
+    "team24"
+    "team25"))
 
 ;; No team switch this quarter
 (define teams-2
